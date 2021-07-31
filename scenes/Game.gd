01ac7294124f1f4efda651efda155abfd3d37d72
Josh Bright <josh.bright@gmail.com>
2021-07-31 12:49:47 -0700

    Added a menu screen when we hit tab

--- scenes/Game.gd
+++ scenes/Game.gd
@@ -2,6 +2,15 @@ extends Node2D
 
 
 onready var world_manager = $WorldManager
+onready var menu = $Menu
 
 func _ready():
 	pass
+
+func _input(event):
+	if event.is_action_pressed("menu"):
+		if menu.visible:
+			print("open menu")
+			menu.visible = false
+		else:
+			menu.visible = true
