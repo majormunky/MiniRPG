ddaa03c1a8f29587abde64ae9190f5167183d832
Josh Bright <josh.bright@gmail.com>
2021-07-31 12:30:46 -0700

    We now load up map information from our data file

--- scenes/WorldManager.gd
+++ scenes/WorldManager.gd
@@ -3,12 +3,24 @@ extends Node2D
 
 var maps = {}
 var world = null
+var map_data: Dictionary
+
+
+func load_json_data(filepath):
+	var json_data
+	var file_data = File.new()
+	file_data.open(filepath, File.READ)
+	json_data = JSON.parse(file_data.get_as_text())
+	return json_data.result
+
 
 func _ready():
-	maps["World"] = preload("res://scenes/maps/World.tscn")
-	maps["FirstTown"] = preload("res://scenes/maps/FirstTown.tscn")
-	world = $World
-	world.connect("location_change", self, "on_location_change")
+	map_data = load_json_data("res://assets/data/world.json")
+	
+	for world_key in map_data:
+		var map_path = "res://" + map_data[world_key].filepath
+		maps[world_key] = load(map_path)
+	load_world("World")
 
 
 func on_town_entered(name):
@@ -23,7 +35,9 @@ func on_location_change(name):
 
 func load_world(world_name):
 	# remove current world
-	remove_child(get_node("World"))
+	var children = get_children()
+	if len(children) > 0:
+		remove_child(children[0])
 	
 	# Create new map that was called for
 	var new_map = maps[world_name].instance()
