035f67711fbdd005d581435fdd3fb4af2afd6dc4
Josh Bright <josh.bright@gmail.com>
2021-07-31 10:58:35 -0700

    Setup the player script to update the animation tree when we move around.  This gives us the idle animation but correctly points in the right directions when we walk

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -2,6 +2,9 @@ extends KinematicBody2D
 
 var velocity = Vector2.ZERO
 var speed = 200
+onready var animation_player = $AnimationPlayer
+onready var animation_tree = $AnimationTree
+
 
 func _physics_process(delta):
 	var input = Vector2.ZERO
@@ -11,8 +14,11 @@ func _physics_process(delta):
 	input = input.normalized()
 	
 	if input != Vector2.ZERO:
+		animation_tree.set("parameters/Idle/blend_position", input);
+		animation_tree.set("parameters/Walk/blend_position", input);
 		velocity = input * speed * delta
 	else:
+		
 		velocity = Vector2.ZERO
 
 	move_and_collide(velocity)
