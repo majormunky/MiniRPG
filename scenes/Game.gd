5da9524ec94ff3e8ddb134f269f16fe65c0b6d1c
Josh Bright <josh.bright@gmail.com>
2021-08-01 08:33:13 -0700

    Added button on the save game menu to save the game during play.  Added the players current location and map to the save game data.

--- scenes/Game.gd
+++ scenes/Game.gd
@@ -47,3 +47,23 @@ func _on_WorldManager_after_map_change():
 	transition_rect.fadeOut()
 	get_tree().paused = false
 	player.update_map_limits()
+
+
+func _on_Menu_save_game():
+	var save_data = {
+		"name": PlayerData.player_name,
+		"type": PlayerData.char_type,
+		"current_location": {
+			"map": world_manager.world_name,
+			"x": player.position.x,
+			"y": player.position.y,
+		}
+	}
+	
+	var save_game_name = "savegame-" + PlayerData.player_name + ".save"
+	var save_game = File.new()
+	if save_game.file_exists("user://" + save_game_name):
+		save_game.open("user://" + save_game_name, File.WRITE)
+		save_game.store_line(to_json(save_data))
+		save_game.close()
+		
--- scenes/Menu.gd
+++ scenes/Menu.gd
@@ -1,5 +1,7 @@
 extends PopupPanel
 
+signal save_game
+
 onready var item_list = $MarginContainer/HBoxContainer/ItemList
 onready var menu_items = {
 	"Status": get_node("MarginContainer/HBoxContainer/StatusMenu"),
@@ -24,6 +26,7 @@ func _on_ItemList_item_selected(index):
 		menu_items["Save"].visible = true
 		menu_items["Status"].visible = false
 		menu_items["Items"].visible = false
+		menu_items["Save"].get_node("MarginContainer/VBoxContainer/SaveMessage").text = ""
 	elif selectedItem == "Items":
 		menu_items["Items"].visible = true
 		menu_items["Status"].visible = false
@@ -34,3 +37,11 @@ func _on_ItemList_item_selected(index):
 		menu_items["Items"].visible = false
 		get_node("MarginContainer/HBoxContainer/StatusMenu/MarginContainer/VBoxContainer/NameLabel").text = "Name: " + PlayerData.player_name
 		get_node("MarginContainer/HBoxContainer/StatusMenu/MarginContainer/VBoxContainer/CharTypeLabel").text = "Class: " + PlayerData.char_type
+
+
+func save_game_complete():
+	menu_items["Save"].get_node("MarginContainer/VBoxContainer/SaveMessage").text = "Save game completed"
+
+
+func _on_SaveButton_pressed():
+	emit_signal("save_game")
