f993f4a5681805a6ab693c6e7a410c9bf8aa71c6
Josh Bright <josh.bright@gmail.com>
2021-07-31 21:58:38 -0700

    We can now load saved games

--- scenes/LoadGame.gd
+++ scenes/LoadGame.gd
@@ -13,3 +13,30 @@ func _ready():
 	
 	for line in saved_game_file_list:
 		save_game_list.add_item(" " + line.split(":")[0])
+
+
+func _on_LoadGameButton_pressed():
+	# load saved game
+	var selected_save = save_game_list.get_selected_items()
+	if len(selected_save) == 0:
+		return
+	
+	selected_save = saved_game_file_list[selected_save[0]]
+	var parts = selected_save.split(":")
+	
+	var save_file = File.new()
+	print(parts[1])
+	if save_file.file_exists("user://" + parts[1]):
+		save_file.open("user://" + parts[1], File.READ)
+		var save_text = save_file.get_as_text()
+		save_file.close()
+		var lines = save_text.split("\n")
+		
+		for line in lines:
+			var line_parts = line.split(":")
+			if line_parts[0] == "name":
+				PlayerData.player_name = line_parts[1]
+			elif line_parts[0] == "type":
+				PlayerData.char_type = line_parts[1]
+		
+		get_tree().change_scene("res://scenes/Game.tscn")
