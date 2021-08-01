c7da4d71c0bd5671f5fe406cf549efc2693c93f8
Josh Bright <josh.bright@gmail.com>
2021-08-01 08:02:05 -0700

    Added comments

--- scenes/CreatePlayer.gd
+++ scenes/CreatePlayer.gd
@@ -17,28 +17,51 @@ func _ready():
 
 
 func create_save_game(user_name, char_type):
+	# prep a file object
 	var save_game = File.new()
+	
+	# check to see if we already have a save with this name
 	if save_game.file_exists("user://savegame-" + user_name + ".save"):
+		# we do, so we need to show an error
 		error_box.text = "Found existing character with that name"
+		
+		# and then bail out
+		return
 	else:
-		print("Unable to find save game, creating new one")
+		# build our save game string
 		var save_game_name = "savegame-" + user_name + ".save"
+		
+		# open the new file for saving
 		save_game.open("user://" + save_game_name, File.WRITE)
+		
+		# save the data to the file and close it
 		save_game.store_line("name:" + user_name)
 		save_game.store_line("type:" + char_type)
 		save_game.close()
 		
+		# we need to register this save game in our text file
+		# this file holds a list of our saved games
+		# we do this due to difficulties listing all the save games in a folder
 		if save_game.file_exists("user://savedgames.save"):
+			# if our save game register already exists
+			# we need to be sure we set our cursor at the end of the file
+			# we also need to open the file as READ_WRITE 
+			# so we can seek
 			save_game.open("user://savedgames.save", File.READ_WRITE)
 			save_game.seek_end()
 		else:
+			# if this is a new file, we can just open it normally
 			save_game.open("user://savedgames.save", File.WRITE)
 
+		# store the save game adn close the file
 		save_game.store_line(user_name + ":" + save_game_name)		
 		save_game.close()
 		
+		# set our global player data with what the user has selected
 		PlayerData.player_name = user_name
 		PlayerData.char_type = char_type
+		
+		# change to the game scene
 		get_tree().change_scene("res://scenes/Game.tscn")
 
 
