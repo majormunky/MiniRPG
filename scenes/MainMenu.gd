b4b4f238bb6f8b0c0e9af32b753e399a37831ac1
Josh Bright <josh.bright@gmail.com>
2021-07-31 21:07:22 -0700

    Setup the new button to go to the create player scene

---
+++ scenes/MainMenu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+onready var new_game_scene = preload("res://scenes/CreatePlayer.tscn")
+
+func _ready():
+	pass
+
+
+func _on_New_Game_pressed():
+	get_tree().change_scene("res://scenes/CreatePlayer.tscn")
+
+
+func _on_Load_Game_pressed():
+	pass # Replace with function body.
