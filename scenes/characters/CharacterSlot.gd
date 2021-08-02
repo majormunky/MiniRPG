d96414f59c4f073ba81d68ebcb6304270344cc0c
Josh Bright <josh.bright@gmail.com>
2021-08-02 11:21:08 -0700

    Updated fields that get used to render the character slot

--- scenes/characters/CharacterSlot.gd
+++ scenes/characters/CharacterSlot.gd
@@ -7,10 +7,10 @@ func update_data(character_data):
 	var level = get_node("MarginContainer/HBoxContainer/MarginContainer/VBoxContainer/Level/Data")
 	var hp = get_node("MarginContainer/HBoxContainer/MarginContainer/VBoxContainer/HP/Data")
 	var experience = get_node("MarginContainer/HBoxContainer/MarginContainer/VBoxContainer/Exp/Data")
-	
+
 	profile.texture = load("res://" + character_data.profile_image)
 	name.text = character_data.character_name
-	job.text = character_data.character_job
+	job.text = character_data.type
 	level.text = str(character_data.level)
-	hp.text = str(character_data.current_health) + "/" + str(character_data.max_health)
+	hp.text = str(character_data.current_hp) + "/" + str(character_data.max_hp)
 	experience.text = str(character_data.experience)
