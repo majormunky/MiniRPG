134ce41be39c17cc619d4115ffb4de15a815d7fa
Josh Bright <josh.bright@gmail.com>
2021-08-02 10:48:39 -0700

    Fixed the character panel to take in a dictionary instead of using a resource

--- scenes/characters/CharacterSlot.gd
+++ scenes/characters/CharacterSlot.gd
@@ -1,8 +1,6 @@
 extends ColorRect
 
-func _ready():
-	var character_data = load("res://assets/characters/Warrior.tres")
-	
+func update_data(character_data):
 	var profile = get_node("MarginContainer/HBoxContainer/CenterContainer/TextureRect")
 	var name = get_node("MarginContainer/HBoxContainer/MarginContainer/VBoxContainer/Name/Data")
 	var job = get_node("MarginContainer/HBoxContainer/MarginContainer/VBoxContainer/Job/Data")
@@ -10,9 +8,8 @@ func _ready():
 	var hp = get_node("MarginContainer/HBoxContainer/MarginContainer/VBoxContainer/HP/Data")
 	var experience = get_node("MarginContainer/HBoxContainer/MarginContainer/VBoxContainer/Exp/Data")
 	
-	
-	profile.texture = character_data.profile_image
-	name.text = "TestName"
+	profile.texture = load("res://" + character_data.profile_image)
+	name.text = character_data.character_name
 	job.text = character_data.character_job
 	level.text = str(character_data.level)
 	hp.text = str(character_data.current_health) + "/" + str(character_data.max_health)
