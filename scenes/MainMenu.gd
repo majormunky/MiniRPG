98829aa03c92bc9abf9cc68a20045d179d81d81a
Josh Bright <josh.bright@gmail.com>
2021-09-14 21:42:10 -0700

    Setup the main menu to be able to be controlled with a controller.  Currently, mouse clicking doesn't work.

--- scenes/MainMenu.gd
+++ scenes/MainMenu.gd
@@ -2,6 +2,9 @@ extends Control
 
 onready var new_game_scene = preload("res://scenes/CreatePlayer.tscn")
 onready var transition = $TransitionRect
+onready var menu_obj = $MarginContainer/VBoxContainer/MarginContainer2/MenuOptions
+
+var current_option = 0
 
 
 func load_json_data(filepath):
@@ -21,9 +24,31 @@ func _ready():
 	var monster_data = load_json_data("res://assets/data/monsters.json")
 	for monster_key in monster_data:
 		GameData.monster_data[monster_key] = monster_data[monster_key]
-	
-	# transition.visible = true
+
 	transition.fadeOut()
+
+
+func _input(event):
+	if event.is_action_pressed("ui_right") or event.is_action_pressed("ui_left"):
+		if current_option == 0:
+			current_option = 1
+		else:
+			current_option = 0
+		update_menu()
+	elif event.is_action_pressed("ui_accept"):
+		if current_option == 0:
+			_on_New_Game_pressed()
+		else:
+			_on_Load_Game_pressed()
+
+
+func update_menu():
+	if current_option == 0:
+		menu_obj.get_node("NewGameLabel/ColorRect").visible = true
+		menu_obj.get_node("LoadGameLabel/ColorRect").visible = false
+	else:
+		menu_obj.get_node("NewGameLabel/ColorRect").visible = false
+		menu_obj.get_node("LoadGameLabel/ColorRect").visible = true
 
 
 func _on_New_Game_pressed():
